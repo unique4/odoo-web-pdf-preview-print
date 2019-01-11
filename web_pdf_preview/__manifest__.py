d2009bb6f8d8c9a31985600dc7514f22b3aba63b
Que Nguyen Thanh <37263237+unique4@users.noreply.github.com>
2019-01-11 15:58:12 +0700

    Create __manifest__.py

---
+++ web_pdf_preview/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Web PDF Report Preview & Print",
+    'version': '1.0',
+    'category': 'Web',
+    'description': """Web PDF Report Preview & Print
+Preview & Print PDF report in your browser.
+* For IE, Adobe Reader is required.
+* For Chrome , nothing is requried.
+* For Firefox, Adobe Reader is required.
+If your brower prevented pop-up window, you should allow it.
+功能：PDF 报表预览
+    """,
+    'author': 'wangbuke@gmail.com',
+    'website': 'http://buke.github.io',
+    'license': 'AGPL-3',
+    'depends': ['web'],
+    'data': [
+        'views/web_pdf_preview.xml',
+    ],
+
+}
